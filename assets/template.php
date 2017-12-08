39f88708c38655e6389c5cab714acbb09a0b1bc6
Stian Hassel Øveråsen <stian@dekode.no>
2017-12-08 07:59:09 +0100

    Template fixes, added element class filters

--- assets/template.php
+++ assets/template.php
@@ -20,7 +20,10 @@ if ( ! defined( 'ABSPATH' ) || ! ( $this instanceof LinkList ) ) {
 }
 
 $container_classes = apply_filters( 'hogan/module/linklist/container_classes', [ 'hogan-linklist-container', $this->type ], $this );
+$container_li_classes = apply_filters( 'hogan/module/linklist/container_li_classes', [], $this );
+
 $list_classes = apply_filters( 'hogan/module/linklist/list_classes', [], $this );
+$list_li_classes = apply_filters( 'hogan/module/linklist/list_li_classes', [], $this );
 
 ?>
 
@@ -40,31 +43,32 @@ $list_classes = apply_filters( 'hogan/module/linklist/list_classes', [], $this )
 				continue;
 			}
 			?>
+			<li class="<?php echo esc_attr( implode( ' ', $container_li_classes ) ); ?>">
+				<?php if ( ! empty( $list['list_heading'] ) ) : ?>
+					<h3><?php echo esc_html( $list['list_heading'] ); ?></h3>
+				<?php endif; ?>
 
-			<?php if ( ! empty( $list['list_heading'] ) ) : ?>
-				<h3><?php echo esc_html( $list['list_heading'] ); ?></h3>
-			<?php endif; ?>
+				<ul class="<?php echo esc_attr( implode( ' ', $list_classes ) ); ?>">
+					<?php $item_counter = 0; foreach ( $items as $item ) : ?>
+						<li class="<?php echo esc_attr( implode( ' ', $list_li_classes ) ); ?>">
+							<?php $unique_item_id = 'link-list-item-' . $this->counter . '-' . $list_counter . '-' . $item_counter; ?>
 
-			<ul class="<?php echo esc_attr( implode( ' ', $list_classes ) ); ?>">
-				<?php $item_counter = 0; foreach ( $items as $item ) : ?>
-					<li>
-						<?php $unique_item_id = 'link-list-item-' . $this->counter . '-' . $list_counter . '-' . $item_counter; ?>
+							<a href="<?php echo esc_url( $item['href'] ); ?>"
+								target="<?php echo esc_attr( $item['target'] ); ?>"
+								rel="<?php echo esc_attr( '_blank' === $item['target'] ? 'noopener noreferrer' : '' ); ?>"
+								aria-describedby="<?php echo esc_attr( $unique_item_id ); ?>"
+								><?php echo esc_html( $item['title'] ); ?></a>
 
-						<a href="<?php echo esc_url( $item['href'] ); ?>"
-							target="<?php echo esc_attr( $item['target'] ); ?>"
-							rel="<?php echo esc_attr( '_blank' === $item['target'] ? 'noopener noreferrer' : '' ); ?>"
-							aria-describedby="<?php echo esc_attr( $unique_item_id ); ?>"
-							><?php echo esc_html( $item['title'] ); ?></a>
-
-						<?php if ( ! empty( $item['description'] ) ) : ?>
-							<span id="<?php echo esc_attr( $unique_item_id ); ?>" class="description"><?php echo esc_html( $item['description'] ); ?></span>
-						<?php endif; ?>
-					</li>
-				<?php
-				$item_counter++;
-				endforeach;
-				?>
-			</ul>
+							<?php if ( ! empty( $item['description'] ) ) : ?>
+								<span id="<?php echo esc_attr( $unique_item_id ); ?>" class="description"><?php echo esc_html( $item['description'] ); ?></span>
+							<?php endif; ?>
+						</li>
+					<?php
+					$item_counter++;
+					endforeach;
+					?>
+				</ul>
+			</li>
 		<?php
 		$list_counter++;
 		endforeach;
