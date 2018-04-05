c837e9322d8be66b8b55e711b419ed4b2a767177
Stian Øveråsen <stian@dekode.no>
2018-04-05 13:49:09 +0200

    Version bump to 1.1.3

--- hogan-linklist.php
+++ hogan-linklist.php
@@ -4,7 +4,7 @@
  * Plugin URI: https://github.com/dekodeinteraktiv/hogan-linklist
  * GitHub Plugin URI: https://github.com/dekodeinteraktiv/hogan-linklist
  * Description: Link List Module for Hogan
- * Version: 1.1.2
+ * Version: 1.1.3
  * Author: Dekode
  * Author URI: https://dekode.no
  * License: GPL-3.0-or-later
